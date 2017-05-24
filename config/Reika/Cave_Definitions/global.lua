3b2fe08287a7abd6e46395028a1f8871f5c8a932
Haggle1996 <Haggle1996@gmail.com>
2017-05-24 16:22:24 -0600

    Remove lighting

--- config/Reika/Cave_Definitions/global.lua
+++ config/Reika/Cave_Definitions/global.lua
@@ -1,17 +1,17 @@
 {
 	type = "global"
-	cave_size = 2.0
+	cave_size = 2.5
 	density = 2.0
 	dungeons = 1.0
 	gen_strongholds = true
 	large_cave_chance = 25.0
-	large_cave_size = 1.0
+	large_cave_size = 1.8
 	large_cave_variance = 6.0
 	lava_caves = true
 	max_y = 256
 	min_y = 0
 	mineshafts = 1.0
-	ravines = 1.0
+	ravines = 1.1
 	vscale = 1.0
 	water_caves = false
 }
